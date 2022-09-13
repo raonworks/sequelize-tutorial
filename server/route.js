5a04c119c86e4e134995ffde9b09d12612e66995
devhong <popodaddy@gmail.com>
2022-09-13 15:12:22 +0900

    associate lazy loading

--- server/route.js
+++ server/route.js
@@ -1,18 +1,32 @@
 import express from 'express';
-import db from './models';
+import models from './models';
 import controller from './controller';
 
-db.sequelize.sync();
+models.sequelize.sync();
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   res.send('home');
 });
 
-router.get('/add', (req, res) => {
+router.get('/add', async (req, res) => {
   controller.addUser();
   res.send('added user');
+});
+
+router.get('/get', async (req, res) => {
+  const tmp = models.User.findAll().then(users => { 
+    return users;
+   });
+
+  const users = await tmp;
+  users.forEach(async user => {
+    const userInfo = await user.getUserInfo();
+    console.log(userInfo);
+  });
+
+  res.send('findAll');
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import router from './route';
-import db from './models';
+
 const app = express();
 
 require('dotenv').config();
